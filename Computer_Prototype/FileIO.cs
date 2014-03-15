c7799a5e3900fb60350778c9854d51a8700e0a50
Christopher Viboux <christ.viboux@gmail.com>
2014-03-15 15:40:45 -0400

    Just adding files.
    They're empty right now. I'll get them filled-up soon enough.

---
+++ Computer_Prototype/FileIO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Prototype
+{
+    class FileIO
+    {
+    }
+}
